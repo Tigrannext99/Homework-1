3942e43e73446c5391455f684831769078d8b5b2
Tigrannext99 <tigran.kr99@gmail.com>
2022-03-07 18:29:07 +0400

    code
    second code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Samara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Программа будет прибавлять два
+             * числа введенные пользователем */
+            Console.WriteLine("Введите ваше имя");
+            String name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте " + name);
+            int num_1;
+            int num_2;
+            Console.WriteLine("Введите два любых числа");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num_1 + num_2;
+            Console.Write("Сумма ваших чисел составляет " + sum);
+        }
+    }
+}
